6b387303320bc279034934d6ac2db0dca7a64600
MadhuSujan93 <madhusujan593@gmail.com>
2023-10-19 12:17:08 +0530

    Changed

--- callbackfunctions.js
+++ callbackfunctions.js
@@ -12,16 +12,62 @@
 // console.log(subres);  
 
 
-const firstNeg=function(num){
-    return num<0;
+// const firstNeg=function(num){
+//     return num<0;
+// }
+// const arr=[1,2,3,5,23,67,-89]
+
+// const result=arr.find(firstNeg)
+// console.log(result);
+
+// const index=arr.findIndex(firstNeg);
+// console.log(index);
+
+
+function getCheese(callback){
+    setTimeout(() => {
+        const cheese="🍕";
+        console.log(`Take the cheese ${cheese}`);
+        callback(cheese);
+    },2000);
 }
-const arr=[1,2,3,5,23,67,-89]
+function makeDough(cheese,callback){
+    setTimeout(() => {
+        const dough=cheese+"🍥";
+        console.log("Take the dough",dough);
+        callback(dough);
 
-const result=arr.find(firstNeg)
-console.log(result);
+    }, 2000);
+}
+function makePizaa(dough,callback){
+    setTimeout(() => {
+        const pizza=dough+"🍘";
+        console.log("here is your pizza",pizza);
+        callback(pizza)
 
-const index=arr.findIndex(firstNeg);
-console.log(index);
+    }, 2000);
+}
+getCheese((ch)=>{
+    console.log("Got the cheese",ch);
+   makeDough(ch,(dough)=>{
+    console.log("Got the dough",dough);
+    makePizaa(dough,(pizza)=>{
+        console.log("Got my pizza Hurray!!",pizza);
+
+    });
+   });
+
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
